9a0cd09cbe1a8ffa3866b62fc4fd5c856222a7f0
yaya-ch <cherifi.yahia@yahoo.fr>
2020-01-02 17:09:38 +0100

    Alex's work checked and corrected

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,41 +3,51 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class AnalyticsCounter {
 	private static int headacheCount = 0;	// initialize to 0
 	private static int rashCount = 0;		// initialize to 0
 	private static int pupilCount = 0;		// initialize to 0
 	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+	public static void main(String args[]) {
+		// Read symptom from an external file
+		try {
+			BufferedReader reader = new BufferedReader (new FileReader("Project02Eclipse/symptoms.txt"));
+			String line = reader.readLine();
+			while (line != null) {
+				System.out.println("symptom from file: " + line);
+				
+				//Counting the number of occurrence of some symptoms
+				if (line.equals("headache")) {
+					headacheCount++;
+				}
+				else if (line.equals("rash")) {
+					rashCount++;
+				}
+				else if (line.contains("pupils")) {
+					pupilCount++;
+				}
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
+				line = reader.readLine();	// get another symptom
 			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
+			reader.close();
+		} catch (IOException e) {
+			System.out.println("Ooops, an error occured");
+			e.printStackTrace();
 		}
 		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+		// Generate a new output file
+		try {
+			FileWriter writer = new FileWriter ("result.out");
+			writer.write("headache: " + headacheCount + "\n");
+			writer.write("rash: " + rashCount + "\n");
+			writer.write("dialated pupils: " + pupilCount + "\n");
+			writer.close();
+		} catch (IOException e) {
+			System.out.println("An error occured.");
+			e.printStackTrace();
+		}
+
 	}
 }
